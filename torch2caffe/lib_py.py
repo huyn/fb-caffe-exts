7ba58bddcf79d4f88191416b333d095ab99b56c6
huyn <pumpkinhuyn@gmail.com>
2017-11-13 20:32:00 +0800

    print conv

--- torch2caffe/lib_py.py
+++ torch2caffe/lib_py.py
@@ -184,9 +184,10 @@ def load(opts):
             # print_all(net.params[name])
             print(net.params[name][0].data[1][0][0][0])
             print(net.params[name][0].data[0][1][0][0])
-            # print(net.params[name][1].data)
+            print(net.params[name][0].data)
+            print("======================")
+            print(net.params[name][1].data)
             # print(net.params[name][0].diff)
-            print(net.params[name])
     return net
 
 
