fe19698a5285946a3c9e62ca37672eab18b218ba
huyn <pumpkinhuyn@gmail.com>
2017-11-15 15:14:54 +0800

    lua demo

--- lua/run.lua
+++ lua/run.lua
@@ -5,7 +5,7 @@ local function main()
 	local input_str={}
 	input_str['input_image']='1.jpg'
 	input_str['output_image']='2.jpg'
-	input_str['model_code']='s528'
+	input_str['model_code']='s528_nn'
 	input_str['max_length']=256
 	transfer.set_model_base_path('/User/huyaonan/caffetest/lua')
 	transfer.transfer_single_image(input_str)
